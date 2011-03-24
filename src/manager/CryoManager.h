7c59d4899b5056bde48412341000b3c45fa872b8
SevenMachines <SevenMachines@yahoo.co.uk>
2011-03-24 11:45:05 +0000

    * Split some common/spacial components off into seperate cryomesh-common library

--- src/manager/CryoManager.h
+++ src/manager/CryoManager.h
@@ -13,7 +13,7 @@ namespace manager {
 
 /**
  * Control manager for cryomesh's. Should be the one point where clients/servers etc interact
- * with the actual mesh
+ * with the actual mesh, databases are managed, and the mesh is created and managed in general
  */
 class CryoManager {
 public:
