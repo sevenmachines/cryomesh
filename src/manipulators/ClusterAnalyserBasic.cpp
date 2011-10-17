1770e2db882de6495501fb4530e4c405fb3c1c2a
SevenMachines <SevenMachines@yahoo.co.uk>
2011-10-17 10:34:52 +0100

    * ClusterAnalyserBasic 	- Implemented analyseCluster() using category matching on short, 	medium and long term trends 	- Added getEnergyVariation to categorise energy within a range 	- Added getFuzzyEnergyVariation to add a random factor into 	which category each data falls

--- src/manipulators/ClusterAnalyserBasic.cpp
+++ src/manipulators/ClusterAnalyserBasic.cpp
@@ -21,11 +21,15 @@ ClusterAnalyserBasic::~ClusterAnalyserBasic() {
 ClusterAnalysisData ClusterAnalyserBasic::analyseCluster(const structures::Cluster & cluster,
 		const std::map<int, std::list<ClusterAnalysisData> > & histories) const {
 
-	const int cluster_node_count = cluster.getNodeMap().getSize();
-	const int cluster_conn_count = cluster.getConnectionMap().getSize();
+	const int current_cluster_node_count = cluster.getNodeMap().getSize();
+	const int current_cluster_conn_count = cluster.getConnectionMap().getSize();
 
 	// current cycle
 	ClusterAnalysisData::RangeEnergy current_range_energy(cluster.getEnergy());
+#ifdef CLUSTERANALYSERBASIC_DEBUG
+	assert(current_range_energy.energy >= -1);
+	assert(current_range_energy.energy <= 1);
+#endif
 	const std::map<int, std::list<ClusterAnalysisData> >::const_iterator it_histories_end = histories.end();
 	std::map<int, std::list<ClusterAnalysisData> >::const_iterator it_histories = histories.begin();
 
@@ -51,31 +55,94 @@ ClusterAnalysisData ClusterAnalyserBasic::analyseCluster(const structures::Clust
 	double conn_creation_weight = 0;
 	double conn_destruction_weight = 0;
 
-	int node_create = 0;
-	int nodes_destroy = 0;
-
-	int conn_create = 0;
-	int conn_destroy = 0;
-
 	//progressive calculation of creation/destruction weights depending on history availability
 
 	if (it_short_range_history != it_histories_end) {
-		// if short term trend is large + then
-		// if short term trend is small + then
-		// if short term trend is small - then
-		// if short term trend is large + then
+		ClusterAnalysisData cad_short_summary = this->calculateRangeEnergies(it_short_range_history->second);
 
 		if (it_medium_range_history != it_histories_end) {
-			// if medium term trend is large + then
-			// if medium term trend is small + then
-			// if medium term trend is small - then
-			// if medium term trend is large + then
+
+			const double medium_base_fraction = 0.01;
+
+			ClusterAnalysisData cad_medium_summary = this->calculateRangeEnergies(it_medium_range_history->second);
+			double val_diff_medium_summary = cad_short_summary.getClusterRangeEnergy().energy
+					- cad_medium_summary.getClusterRangeEnergy().energy;
+			//	double min_diff_medium_summary = cad_short_summary.getClusterRangeEnergy().energy
+			//		- cad_medium_summary.getClusterRangeEnergy().energyMin;
+			//double max_diff_medium_summary = cad_short_summary.getClusterRangeEnergy().energy
+			//		- cad_medium_summary.getClusterRangeEnergy().energyMax;
+
+			EnergyVariation medium_variation = this->getFuzzyEnergyVariation(val_diff_medium_summary, 1.0, 0.01);
+			if (medium_variation <= EnergyVariation::HIGH_NEGATIVE) {
+				// kill/create a lot of nodes
+				node_destruction_weight += medium_base_fraction;
+				node_creation_weight += medium_base_fraction;
+			} else if (medium_variation == EnergyVariation::MEDIUM_NEGATIVE) {
+				// kill/create some nodes
+				node_destruction_weight += medium_base_fraction / 2.0;
+				node_creation_weight += medium_base_fraction / 2.0;
+			} else if (medium_variation == EnergyVariation::SMALL_NEGATIVE) {
+				// kill/create some connections
+				conn_destruction_weight += medium_base_fraction;
+				conn_creation_weight += medium_base_fraction;
+			} else if (medium_variation == EnergyVariation::STAGNANT_NEGATIVE) {
+				// kill/create a few connections
+				conn_destruction_weight += medium_base_fraction / 2.0;
+				conn_creation_weight += medium_base_fraction / 2.0;
+			} else if ((medium_variation == EnergyVariation::ZERO)
+					|| (medium_variation == EnergyVariation::STAGNANT_POSITIVE)) {
+				//create a few nodes
+				node_creation_weight += medium_base_fraction / 2.0;
+			} else if (medium_variation == EnergyVariation::SMALL_POSITIVE) {
+				// create a few connections
+				conn_creation_weight += medium_base_fraction / 2.0;
+			} else if (medium_variation == EnergyVariation::MEDIUM_POSITIVE) {
+				// create a very few connections
+				conn_creation_weight += medium_base_fraction / 4.0;
+			} else if (medium_variation >= EnergyVariation::HIGH_POSITIVE) {
+				// everything good, do nothing
+			}
 
 			if (it_long_range_history != it_histories_end) {
-				// if long term trend is large + then
-				// if long term trend is small + then
-				// if long term trend is small - then
-				// if long term trend is large + then
+				ClusterAnalysisData cad_long_summary = this->calculateRangeEnergies(it_long_range_history->second);
+				double val_diff_long_summary = cad_medium_summary.getClusterRangeEnergy().energy
+						- cad_long_summary.getClusterRangeEnergy().energy;
+				//	double min_diff_long_summary = cad_medium_summary.getClusterRangeEnergy().energy
+				//			- cad_long_summary.getClusterRangeEnergy().energyMin;
+				//	double max_diff_long_summary = cad_medium_summary.getClusterRangeEnergy().energy
+				//			- cad_long_summary.getClusterRangeEnergy().energyMax;
+
+				const double long_base_fraction = 0.1;
+				EnergyVariation long_variation = this->getFuzzyEnergyVariation(val_diff_long_summary, 1.0, 0.01);
+				if (long_variation <= EnergyVariation::HIGH_NEGATIVE) {
+					// kill/create a lot of nodes
+					node_destruction_weight += long_base_fraction;
+					node_creation_weight += long_base_fraction;
+				} else if (long_variation == EnergyVariation::MEDIUM_NEGATIVE) {
+					// kill/create some nodes
+					node_destruction_weight += long_base_fraction / 2.0;
+					node_creation_weight += long_base_fraction / 2.0;
+				} else if (long_variation == EnergyVariation::SMALL_NEGATIVE) {
+					// kill/create some connections
+					conn_destruction_weight += long_base_fraction;
+					conn_creation_weight += long_base_fraction;
+				} else if (long_variation == EnergyVariation::STAGNANT_NEGATIVE) {
+					// kill/create a few connections
+					conn_destruction_weight += long_base_fraction / 2.0;
+					conn_creation_weight += long_base_fraction / 2.0;
+				} else if ((long_variation == EnergyVariation::ZERO)
+						|| (long_variation == EnergyVariation::STAGNANT_POSITIVE)) {
+					//create a few nodes
+					node_creation_weight += long_base_fraction / 2.0;
+				} else if (long_variation == EnergyVariation::SMALL_POSITIVE) {
+					// create a few connections
+					conn_creation_weight += long_base_fraction / 2.0;
+				} else if (long_variation == EnergyVariation::MEDIUM_POSITIVE) {
+					// create a very few connections
+					conn_creation_weight += long_base_fraction / 4.0;
+				} else if (long_variation >= EnergyVariation::HIGH_POSITIVE) {
+					// everything good, do nothing
+				}
 			} // it_long_range_history
 		} // it_medium_range_history
 	} // it_short_range_history
@@ -87,12 +154,26 @@ ClusterAnalysisData ClusterAnalyserBasic::analyseCluster(const structures::Clust
 	assert((conn_destruction_weight >= 0) && (conn_destruction_weight <= 1));
 
 	// finally calculate what restructuring to do
-	node_create = node_creation_weight * cluster_node_count;
-	nodes_destroy = node_destruction_weight * cluster_node_count;
-	conn_create = conn_creation_weight * cluster_conn_count;
-	conn_destroy = conn_destruction_weight * cluster_conn_count;
+	int node_create = node_creation_weight * current_cluster_node_count;
+	int nodes_destroy = node_destruction_weight * current_cluster_node_count;
 
-#ifdef CLUSTERANALYSERBASIC_DEBUG
+	int conn_create = conn_creation_weight * current_cluster_conn_count;
+	int conn_destroy = conn_destruction_weight * current_cluster_conn_count;
+
+	// force some effect when we get fractions less than 1
+	if (node_creation_weight > 0) {
+		node_create = std::max(node_create, 1);
+	}
+	if (node_destruction_weight > 0) {
+		nodes_destroy = std::max(nodes_destroy, 1);
+	}
+	if (conn_creation_weight > 0) {
+		conn_create = std::max(conn_create, 1);
+	}
+	if (conn_destruction_weight > 0) {
+		conn_destroy = std::max(conn_destroy, 1);
+	}
+
 	if (it_short_range_history == it_histories_end) {
 		// check we did nothing
 		assert(node_create == 0);
@@ -100,7 +181,6 @@ ClusterAnalysisData ClusterAnalyserBasic::analyseCluster(const structures::Clust
 		assert(conn_create == 0);
 		assert(conn_destroy == 0);
 	}
-#endif
 	return ClusterAnalysisData(current_range_energy, node_creation_weight, node_destruction_weight,
 			conn_creation_weight, conn_destruction_weight, node_create, nodes_destroy, conn_create, conn_destroy);
 }
@@ -112,13 +192,11 @@ ClusterAnalysisData ClusterAnalyserBasic::calculateRangeEnergies(const std::list
 	double node_destruction_weight = 0;
 	double conn_creation_weight = 0;
 	double conn_destruction_weight = 0;
-
 	// get totals of these
 	int node_create = 0;
 	int nodes_destroy = 0;
 	int conn_create = 0;
 	int conn_destroy = 0;
-
 	// forall in history
 	{
 		int count = 0;
@@ -131,7 +209,6 @@ ClusterAnalysisData ClusterAnalyserBasic::calculateRangeEnergies(const std::list
 			node_destruction_weight = ad.getNodeDestructionWeight();
 			conn_creation_weight = ad.getConnectionCreationWeight();
 			conn_destruction_weight = ad.getConnectionDestructionWeight();
-
 			node_create = ad.getNodesToCreate();
 			nodes_destroy = ad.getNodesToDestroy();
 			conn_create = ad.getConnectionsToCreate();
@@ -141,19 +218,15 @@ ClusterAnalysisData ClusterAnalyserBasic::calculateRangeEnergies(const std::list
 		++count;
 		while (it_history != it_history_end) {
 			const ClusterAnalysisData & ad = *it_history;
-
 			cluster_range_energy += ad.getClusterRangeEnergy();
-
 			node_creation_weight += ad.getNodeCreationWeight();
 			node_destruction_weight += ad.getNodeDestructionWeight();
 			conn_creation_weight += ad.getConnectionCreationWeight();
 			conn_destruction_weight += ad.getConnectionDestructionWeight();
-
 			node_create += ad.getNodesToCreate();
 			nodes_destroy += ad.getNodesToDestroy();
 			conn_create += ad.getConnectionsToCreate();
 			conn_destroy += ad.getConnectionsToDestroy();
-
 			++count;
 			++it_history;
 		}
@@ -167,5 +240,43 @@ ClusterAnalysisData ClusterAnalyserBasic::calculateRangeEnergies(const std::list
 			conn_creation_weight, conn_destruction_weight, node_create, nodes_destroy, conn_create, conn_destroy);
 }
 
+ClusterAnalyserBasic::EnergyVariation ClusterAnalyserBasic::getEnergyVariation(double d, double range) const {
+	EnergyVariation varation;
+	if (d > range) {
+		varation = OUT_OF_RANGE_POSITIVE;
+	} else if (d < -range) {
+		varation = OUT_OF_RANGE_NEGATIVE;
+	} else if (d >= 0.8 * range) {
+		varation = HIGH_POSITIVE;
+	} else if (d >= 0.5 * range) {
+		varation = MEDIUM_POSITIVE;
+
+	} else if (d >= 0.1 * range) {
+		varation = SMALL_POSITIVE;
+	} else if (d > 0 * range) {
+		varation = STAGNANT_POSITIVE;
+	} else if (d == 0) {
+		varation = ZERO;
+
+	} else if (d <= -0.8 * range) {
+		varation = HIGH_NEGATIVE;
+	} else if (d <= -0.5 * range) {
+		varation = MEDIUM_NEGATIVE;
+	} else if (d <= -0.1 * range) {
+		varation = SMALL_NEGATIVE;
+	} else if (d < 0) {
+		varation = STAGNANT_NEGATIVE;
+	} else {
+		// should never be here
+		assert(false);
+	}
+	return varation;
+}
+ClusterAnalyserBasic::EnergyVariation ClusterAnalyserBasic::getFuzzyEnergyVariation(double d, double range,
+		double fuzz_factor) const {
+	const double fuzzy_factor = common::Maths::getRandomDouble(-fuzz_factor * range, fuzz_factor * range);
+	double fuzzy_var = d + fuzzy_factor;
+	return (this->getEnergyVariation(fuzzy_var, range));
+}
 } /* namespace manipulators */
 } /* namespace cryomesh */
--- src/manipulators/ClusterAnalyserBasic.h
+++ src/manipulators/ClusterAnalyserBasic.h
@@ -15,12 +15,28 @@ namespace manipulators {
 
 class ClusterAnalyserBasic: public cryomesh::manipulators::IClusterAnalyser {
 public:
+	enum EnergyVariation{
+		OUT_OF_RANGE_POSITIVE = 5,
+		HIGH_POSITIVE = 4,
+		MEDIUM_POSITIVE = 3,
+		SMALL_POSITIVE = 2,
+		STAGNANT_POSITIVE = 1,
+		ZERO = 0,
+		STAGNANT_NEGATIVE  = -1,
+		SMALL_NEGATIVE = -2,
+		MEDIUM_NEGATIVE = -3,
+		HIGH_NEGATIVE  = -4,
+		OUT_OF_RANGE_NEGATIVE = -5
+	};
 	ClusterAnalyserBasic();
 	virtual ~ClusterAnalyserBasic();
 	virtual ClusterAnalysisData analyseCluster(const structures::Cluster & cluster,
-			const std::map<int, std::list<ClusterAnalysisData> > & histories)const;
-	virtual ClusterAnalysisData calculateRangeEnergies(const std::list<ClusterAnalysisData> & history)const;
+			const std::map<int, std::list<ClusterAnalysisData> > & histories) const;
+	virtual ClusterAnalysisData calculateRangeEnergies(const std::list<ClusterAnalysisData> & history) const;
 
+protected:
+	virtual EnergyVariation getFuzzyEnergyVariation(double d, double range,  double fuzz_factor)const;
+	virtual EnergyVariation getEnergyVariation(double d, double range)const;
 };
 
 } /* namespace manipulators */
